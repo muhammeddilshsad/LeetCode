2c7941bc73d8cf6285f767dec0538388cf83392e
muhammeddilshsad <diludilshazz42@gmail.com>
2025-05-07 10:05:14 +0530

    Time: 44 ms (33.96%), Space: 53.4 MB (58.46%) - LeetHub

--- 2734-array-prototype-last/2734-array-prototype-last.js
+++ 2734-array-prototype-last/2734-array-prototype-last.js
@@ -2,14 +2,10 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
+    return this.length === 0 ? -1 : this[this.length - 1];
 
-    if(this.length==0){
-        return-1;
-    }
-   return this[this.length - 1];
     
 };
-
 
 /**
  * const arr = [1, 2, 3];
