76b00cf783f9061d29694a3b0a1e8f5fc76808bc
muhammeddilshsad <diludilshazz42@gmail.com>
2025-06-12 10:01:45 +0530

    Time: 81 ms (20.37%), Space: 63 MB (49.1%) - LeetHub

--- 2758-check-if-object-instance-of-class/2758-check-if-object-instance-of-class.js
+++ 2758-check-if-object-instance-of-class/2758-check-if-object-instance-of-class.js
@@ -4,19 +4,20 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    while(obj!=null)
-    {
-        if(obj.constructor === classFunction)
-        {
+     if (obj == null || typeof classFunction !== 'function') return false;
+
+    let prototype = Object.getPrototypeOf(obj);
+
+    while (prototype !== null) {
+        if (prototype.constructor === classFunction) {
             return true;
         }
-        obj= Object.getPrototypeOf(obj);
+        prototype = Object.getPrototypeOf(prototype);
     }
+
     return false;
-   
     
 };
-
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
